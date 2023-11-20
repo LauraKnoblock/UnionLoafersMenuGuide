3207c10f3d7502e33ed556a4c863e30155be6a88
Your Name <laura.e.knoblock@gmail.com>
2023-11-20 16:23:02 -0600

    rerouting to home menu

--- src/main/java/com/UnionLoafers/MenuGuide/controllers/ItemController.java
+++ src/main/java/com/UnionLoafers/MenuGuide/controllers/ItemController.java
@@ -4,7 +4,9 @@ package com.UnionLoafers.MenuGuide.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,15 +15,21 @@ import java.util.List;
 @RequestMapping("items")
 public class ItemController {
 
+  private static List<String> items = new ArrayList<>();
+
   @GetMapping
   public String  displayAllItems(Model model) {
-    List<String> items = new ArrayList<>();
-    items.add("Little Gem Salad");
     model.addAttribute("items", items);
             return "items/index";
   }
   @GetMapping("create")
   public String renderCreateItem() {
     return "items/create";
+  }
+
+  @PostMapping("create")
+  public String createItem(@RequestParam String itemName) {
+    items.add(itemName);
+    return "redirect:/items";
   }
 }
