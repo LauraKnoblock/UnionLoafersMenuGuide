2e0be98126c4cbbe83534ef379891364df764c1b
Your Name <laura.e.knoblock@gmail.com>
2023-11-20 12:36:27 -0600

    playing with paths

--- src/main/java/com/UnionLoafers/MenuGuide/controllers/HomeController.java
+++ src/main/java/com/UnionLoafers/MenuGuide/controllers/HomeController.java
@@ -1,42 +1,17 @@
 package com.UnionLoafers.MenuGuide.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HomeController {
 
-
-//  @ResponseBody
-//  @GetMapping("hello")
-//  public String hello() {
-//    return "Hello, Barry & Pepper!";
-//  }
-
-
-  @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value="hello")
-  @ResponseBody
-  public String helloWithQueryParam(@RequestParam String name) {
-    return "Hello, " + name + "!";
-  }
-
-  // handles requests of the form /hello/LaunchCode
-  @GetMapping("hello/{name}")
-  @ResponseBody
-  public String helloWithPathParam(@PathVariable String name){
-    return "Hello, " + name + "!";
-  }
-
-  @GetMapping("form")
-  @ResponseBody
-  public String helloForm() {
-    return "<html>" +
-            "<body>" +
-            "<form action='hello' method='post'>" + // submit request to /hello
-            "<input type='text' name='name'>" +
-            "<input type='submit' value='Greet me!'/>" +
-            "</form>" +
-            "</body>" +
-            "</html>";
+  @GetMapping
+  public String index() {
+    return "index";
   }
 }
---
+++ src/main/java/com/UnionLoafers/MenuGuide/controllers/ItemController.java
@@ -0,0 +1,23 @@
+package com.UnionLoafers.MenuGuide.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("items")
+public class ItemController {
+
+  @GetMapping
+  public String  displayAllItems(Model model) {
+    List<String> items = new ArrayList<>();
+    items.add("Little Gem Salad");
+    model.addAttribute("items", items);
+            return "items/index";
+  }
+}
